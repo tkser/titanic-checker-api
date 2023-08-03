efe2e21821bc10e042185ba453204f1d9498f747
tkser <tkser.edu@gmail.com>
2023-08-03 17:20:18 +0900

    add: cors

--- app/main.py
+++ app/main.py
@@ -1,10 +1,24 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.judge import judge_csv
 from api.data import df_initialize
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "https://titanic-checker.pages.dev",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 async def startup_event():
@@ -15,7 +29,7 @@ async def index():
     return {"message": "Hello, Titanic!"}
 
 @app.post("/judge")
-async def judge(csv_data: str):
+async def post_judge(csv_data: str):
     return await judge_csv(csv_data)
 
 def main():
