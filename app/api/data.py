9f113c27d0c6d978e7a5add4f57c87aedcd18bad
tkser <tkser.edu@gmail.com>
2023-08-03 18:06:17 +0900

    fix: data.py

--- app/api/data.py
+++ app/api/data.py
@@ -1,7 +1,9 @@
+import os
 import pandas as pd
 
 test_df = None
 
 async def df_initialize():
     global test_df
-    test_df = pd.read_csv("../data/test.csv")
+    data_path = os.path.join(os.path.dirname(__file__), "../../data/test_true.csv")
+    test_df = pd.read_csv(data_path)
